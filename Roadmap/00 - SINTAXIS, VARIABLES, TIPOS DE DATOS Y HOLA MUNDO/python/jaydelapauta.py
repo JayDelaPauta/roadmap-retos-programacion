3e1a7679cfadafe71f7e1ddf37b17950b57e5135
JayDelaPauta <johanmpj10@gmail.com>
2024-05-13 19:27:35 -0400

    jaydelapauta.py
    mi primera reslución *-*
    EJERCICIO #1

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/jaydelapauta.py
@@ -0,0 +1,47 @@
+# Pagina Web: https://www.python.org/
+
+# COMENTARIOS (se usa el símbolo # para dejar comentarios)
+
+"""
+Esto da referencia a un comentario en multiples lineas codigo
+"""
+
+'''
+Otra manera de comentar multiples lineas de codigo
+'''
+
+
+# VARIABLES Y CONSTANTES
+
+
+my_variable = "mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+MY_CONSTANT = "La constante"
+'''En el caso de python una constante 
+es mas una convencion que una restriccion, y estas se representan poniendo su nombre en mayusculas'''
+
+
+# Enteros (int)
+my_int = 72
+
+# Flotantes (float)
+my_float = 3,5
+
+# Booleanos (bool)
+my_bool = True
+my_bool = False
+
+my_string = "Mi cadena de texto"
+my_other_string = "Mi otra cadena de texto"
+
+# Nulos (nonetype)
+valor_nulo = None
+
+# Mensaje saludando con el lenguaje que quiero aprender
+print("¡Hola, Python!")
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_string))
